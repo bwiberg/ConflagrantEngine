881c413ebe22c37c68884375fab2a14546a9928f
Benjamin Wiberg <benjaminwiberg@gmail.com>
2017-11-03 11:22:20 +0100

    Delete conflagrant/ecs files

--- src/conflagrant/components/MeshComponent.hh
+++ src/conflagrant/components/MeshComponent.hh
@@ -4,14 +4,13 @@
 
 #pragma once
 
-#include <conflagrant/ecs/Component.hh>
 #include <conflagrant/GL.hh>
 
 namespace cfl {
 /**
  * @brief Component that references a Mesh in OpenGL memory.
  */
-struct MeshComponent : public ecs::Component {
+struct MeshComponent {
 
 };
 }
--- src/conflagrant/ecs/internal/ComponentType.cc
+++
@@ -1,9 +0,0 @@
-/**
- * Created by bwiberg on 9/11/17.
- */
-
-#include "ComponentType.hh"
-
-std::array<cfl::ecs::internal::ComponentType::Data, CFL_MAX_COMPONENT_TYPES> cfl::ecs::internal::ComponentType::ComponentData;
-
-cfl::ecs::internal::component_id_t cfl::ecs::internal::ComponentType::NumRegisteredTypes = 0;
